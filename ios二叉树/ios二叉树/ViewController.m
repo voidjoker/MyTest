061cc801fcd55a01547dd2629da07f78a642e77e
zhangjj <zjjevil@outlook.com>
2017-05-01 16:22:18 +0800

    ok

--- ios二叉树/ios二叉树/ViewController.m
+++ ios二叉树/ios二叉树/ViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"test add git for study github");
-    NSLog(@"log and reloa");
+    NSLog(@"log and reload zjzjzj");
 }
 
 - (void)didReceiveMemoryWarning {
